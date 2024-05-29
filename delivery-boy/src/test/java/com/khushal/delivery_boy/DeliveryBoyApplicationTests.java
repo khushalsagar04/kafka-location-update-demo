736b3c5686f5427a4ed89824e8d19ce202bf5bec
Khushal Sagar Kumawat <khushalsagar04@gmail.com>
2024-05-29 23:18:17 +0530

    initial project built for location tracker in kafka

---
+++ delivery-boy/src/test/java/com/khushal/delivery_boy/DeliveryBoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khushal.delivery_boy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryBoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
